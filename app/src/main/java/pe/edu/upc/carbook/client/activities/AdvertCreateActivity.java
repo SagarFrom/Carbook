1ce7baec72d105b0808206f50b1fc758827db254
Richard Rondan <rondanrichard@gmail.com>
2016-11-21 23:45:29 -0500

    Carbook-Adverts-Client: Creando Anuncio - 25% - Parte Visual

--- app/src/main/java/pe/edu/upc/carbook/client/activities/AdvertCreateActivity.java
+++ app/src/main/java/pe/edu/upc/carbook/client/activities/AdvertCreateActivity.java
@@ -1,15 +1,33 @@
 package pe.edu.upc.carbook.client.activities;
 
+import android.app.DatePickerDialog;
+
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.InputType;
 import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import java.util.Calendar;
+import java.util.Locale;
+import java.text.SimpleDateFormat;
 import pe.edu.upc.carbook.R;
 
-public class AdvertCreateActivity extends AppCompatActivity {
+public class AdvertCreateActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private EditText toDateEditText;
+    private EditText descriptionEditText;
+
+    private DatePickerDialog toDatePickerDialog;
+
+    private SimpleDateFormat dateFormatter;
+    final Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +35,19 @@ public class AdvertCreateActivity extends AppCompatActivity {
         setContentView(R.layout.client_activity_advert_create);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        dateFormatter = new SimpleDateFormat("dd-MM-yyyy",Locale.US);
+
+        findViewsById();
+        setDateTimeField();
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -26,7 +57,38 @@ public class AdvertCreateActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    private void findViewsById() {
+        toDateEditText = (EditText) findViewById(R.id.toDateEditText);
+        toDateEditText.setInputType(InputType.TYPE_NULL);
+        toDateEditText.requestFocus();
+    }
+
+    private void setDateTimeField(){
+        toDateEditText.setOnClickListener(this);
+
+        Calendar newCalendar = Calendar.getInstance();
+        toDatePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                Calendar newDate = Calendar.getInstance();
+                newDate.set(year, month, dayOfMonth);
+                toDateEditText.setText(dateFormatter.format(newDate.getTime()));
+                //Aca cambiar la data del Año
+                Toast toast = Toast.makeText(context,"Fecha Fin: "+ newDate.getTime().toString() ,Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        },newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
+
+    }
+
+    @Override
+    public void onClick(View view){
+        if(view == toDateEditText){
+            toDatePickerDialog.show();
+        }
     }
 
 }
---
+++ app/src/main/java/pe/edu/upc/carbook/share/models/Car.java
@@ -0,0 +1,81 @@
+package pe.edu.upc.carbook.share.models;
+
+/**
+ * Created by usuario on 21/11/2016.
+ */
+
+public class Car {
+    private String carId;
+    private String clientId;
+    private String brand;
+    private String model;
+    private String status;
+    private String plateNumber;
+    private String firstPhotoUrl;
+
+    public Car() {
+    }
+
+    public String getCarId() {
+        return carId;
+    }
+
+    public void setCarId(String carId) {
+        this.carId = carId;
+    }
+
+    public String getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(String clientId) {
+        this.clientId = clientId;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getPlateNumber() {
+        return plateNumber;
+    }
+
+    public void setPlateNumber(String plateNumber) {
+        this.plateNumber = plateNumber;
+    }
+
+    public String getFirstPhotoUrl() {
+        return firstPhotoUrl;
+    }
+
+    public void setFirstPhotoUrl(String firstPhotoUrl) {
+        this.firstPhotoUrl = firstPhotoUrl;
+    }
+
+    public String getCarFullName(){
+        return this.brand + " " + this.model;
+    }
+
+    
+
+}
